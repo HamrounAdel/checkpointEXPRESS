b10df88233d2a5a839dfc44a3da59d973ed4fc73
HamrounAdel <HamrounAdel@email.fr>
2023-11-01 09:57:22 +0100

    finish commit

--- middel/date.js
+++ middel/date.js
@@ -1,8 +1,8 @@
 const datefunc=(req,res,next)=>{
  const date= new Date();
  let h=date.getHours()
-
- if(!(h>=9   && h<=17)){
+ let d=date.getDay()
+ if((!(h>=9   && h<=17)) || (!(d>=1 && d<=5))){
  res.send('pas disponible pour le momment')
  }next()
 }
